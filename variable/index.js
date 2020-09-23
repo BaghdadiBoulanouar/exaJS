be9f79675ed58614ef993c878c02e82f9ce9eb5f
Baghdadi <b.baghdadi93@gmail.com>
2020-09-23 11:36:14 +0200

    feat(variable): exercice about variable

---
+++ variable/index.js
@@ -0,0 +1,45 @@
+alert('test');
+
+'use strict';
+/**
+ * Exercice 1
+ */
+ // My digital School Forever
+ 
+ /**
+ * Exercice 2
+ */
+ /*My 
+ Digital 
+ School 
+ forever 
+ every 
+ day
+ */
+
+  /**
+ * Exercice 3
+ */
+var string = '42';
+var number = 42;
+var float = 42;
+var bool = true | false;
+var array = [42];
+var date = 42;
+var object = "42";
+var undefined;
+var null;
+ /**
+ * Exercice 4
+ */
+var my42count = 'quarante-deux';
+console.log(`${my42count} ${my42count.length}`);
+ /**
+ * Exercice 5
+ */
+var test = date2 || 42;
+console.log(test);
+ /**
+ * Exercice 6
+ */
+var myArray42 = []
