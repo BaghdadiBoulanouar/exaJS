3b4474f1533437d8ae95a8692b2e98e24ea66a7b
Baghdadi <b.baghdadi93@gmail.com>
2020-09-23 12:13:13 +0200

    feat(variable): finished exercice

--- variable/index.js
+++ variable/index.js
@@ -27,7 +27,7 @@ var bool = true;
 var array = [42];
 var date = newDate(4241,12, 24, 42);
 var datetest;
-var object = {"42"; 42};
+var object = {"42", 42};
 var undefin = undefined;
 var und2;
 var empty = null;
@@ -46,4 +46,24 @@ console.log(test);
  /**
  * Exercice 6
  */
-var myArray42 = []
+var myArray42 = ['q','u','a','r','a','n','t','e','-','d','e','u','x']
+console.log(myArray42);
+ /**
+ * Exercice 7
+ */
+var myArray42Len = myArray42.lenght;
+console.log(myArray42Len);
+ /**
+ * Exercice 8
+ */
+console.log(myArray42.join('') + 'La grande reponse sur la vie, l\' univers et le reste!');
+ /**
+ * Exercice 9
+ */
+var rand = Math.floor((Math.random() * (42)) + 1) === 42;
+console.log(rand);
+ /**
+ * Exercice 10
+ */
+ var my42Type = [typeof string, typeof number];
+ console.log(my42Type);
